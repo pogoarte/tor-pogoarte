561fe27cae25765d79bb270a647080d8bcdffd18
pogoarte <98662243+pogoarte@users.noreply.github.com>
2024-02-07 20:57:57 +0100

    Update Raspy_PiZero_Tor_Dietpi_1.0.sh

--- Raspy_PiZero_Tor_Dietpi_1.0.sh
+++ Raspy_PiZero_Tor_Dietpi_1.0.sh
@@ -2,8 +2,7 @@
 #
 clear
 echo""
-echo "# - Install and configure a Tor BRIDGE for Raspberry Pi Zero W and DietPi v9.0.2."
-echo "# - Need dietpi default user exist."
+echo "# - Install and configure a Tor BRIDGE for Raspberry Pi Zero W and Dietpi 9.0.2."
 echo "# - Compile tor and obfs4proxy from git."
 echo "# - Generate Tor BRIDGE config with socks disable and monitor enable (nyx)."
 echo "# - If Tor BRIDGE is behind a FIREWALL or NAT, make sure to open or forward TCP port 9001 and 9002."
@@ -17,6 +16,8 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+adduser --quiet --system --disabled-password --shell /bin/false --group debian-tor
+
 apt update
 apt install -y automake build-essential curl libevent-dev libssl-dev liblzma-dev libzstd-dev nyx pkg-config vnstat zlib1g-dev
 curl -L https://dist.torproject.org/tor-0.4.8.10.tar.gz | tar zxf -
@@ -24,36 +25,36 @@ cd tor-0.4.8.10
 ./configure --disable-asciidoc
 make
 
-mkdir /home/dietpi/tor
-mkdir /home/dietpi/tor/bin
-mkdir /home/dietpi/tor/data
-mkdir /home/dietpi/tor/etc
-mkdir /home/dietpi/tor/log
-chmod 700 /home/dietpi/tor
-chmod 700 /home/dietpi/tor/bin
-chmod 700 /home/dietpi/tor/data
-chmod 700 /home/dietpi/tor/etc
-chmod 700 /home/dietpi/tor/log
-install -c -m 700 src/app/tor src/tools/tor-resolve src/tools/tor-print-ed-signing-cert src/tools/tor-gencert '/home/dietpi/tor/bin'
-install -c -m 600 src/config/geoip src/config/geoip6 '/home/dietpi/tor/data'
+mkdir /home/debian-tor/tor
+mkdir /home/debian-tor/tor/bin
+mkdir /home/debian-tor/tor/data
+mkdir /home/debian-tor/tor/etc
+mkdir /home/debian-tor/tor/log
+chmod 700 /home/debian-tor/tor
+chmod 700 /home/debian-tor/tor/bin
+chmod 700 /home/debian-tor/tor/data
+chmod 700 /home/debian-tor/tor/etc
+chmod 700 /home/debian-tor/tor/log
+install -c -m 700 src/app/tor src/tools/tor-resolve src/tools/tor-print-ed-signing-cert src/tools/tor-gencert '/home/debian-tor/tor/bin'
+install -c -m 600 src/config/geoip src/config/geoip6 '/home/debian-tor/tor/data'
 
 echo "[Unit]" > /etc/systemd/system/tor.service
 echo "Description=TOR Anonymizing Overlay Network" >> /etc/systemd/system/tor.service
 echo "After=network.target" >> /etc/systemd/system/tor.service
 echo "" >> /etc/systemd/system/tor.service
 echo "[Service]" >> /etc/systemd/system/tor.service
-echo "User=dietpi" >> /etc/systemd/system/tor.service
-echo "Group=dietpi" >> /etc/systemd/system/tor.service
+echo "User=debian-tor" >> /etc/systemd/system/tor.service
+echo "Group=debian-tor" >> /etc/systemd/system/tor.service
 echo "" >> /etc/systemd/system/tor.service
 echo "PrivateTmp=yes" >> /etc/systemd/system/tor.service
 echo "" >> /etc/systemd/system/tor.service
 echo "PermissionsStartOnly=true" >> /etc/systemd/system/tor.service
 echo "" >> /etc/systemd/system/tor.service
-echo "ExecStart=/home/dietpi/tor/bin/tor -f /home/dietpi/tor/etc/torrc" >> /etc/systemd/system/tor.service
+echo "ExecStart=/home/debian-tor/tor/bin/tor -f /home/debian-tor/tor/etc/torrc" >> /etc/systemd/system/tor.service
 echo "" >> /etc/systemd/system/tor.service
 echo "ExecReload=/usr/bin/kill -HUP $MAINPID" >> /etc/systemd/system/tor.service
 echo "" >> /etc/systemd/system/tor.service
-echo "PIDFile=/home/dietpi/tor/tor.pid" >> /etc/systemd/system/tor.service
+echo "PIDFile=/home/debian-tor/tor/tor.pid" >> /etc/systemd/system/tor.service
 echo "KillSignal=SIGINT" >> /etc/systemd/system/tor.service
 echo "LimitNOFILE=8192" >> /etc/systemd/system/tor.service
 echo "PrivateDevices=yes" >> /etc/systemd/system/tor.service
@@ -61,43 +62,43 @@ echo "" >> /etc/systemd/system/tor.service
 echo "[Install]" >> /etc/systemd/system/tor.service
 echo "WantedBy=multi-user.target" >> /etc/systemd/system/tor.service
 
-echo "DataDirectory /home/dietpi/tor/data" > /home/dietpi/tor/etc/torrc
-echo "GeoIPFile /home/dietpi/tor/data/geoip" >> /home/dietpi/tor/etc/torrc
-echo "GeoIPv6File /home/dietpi/tor/data/geoip6" >> /home/dietpi/tor/etc/torrc
-echo "Log notice file /home/dietpi/tor/log/notices.log" >> /home/dietpi/tor/etc/torrc
-echo "PidFile /home/dietpi/tor/tor.pid" >> /home/dietpi/tor/etc/torrc
-echo "SocksPort 0" >> /home/dietpi/tor/etc/torrc
-echo "BridgeRelay 1" >> /home/dietpi/tor/etc/torrc
-echo "RunAsDaemon 1" >> /home/dietpi/tor/etc/torrc
-echo "AvoidDiskWrites 1" >> /home/dietpi/tor/etc/torrc
-echo "ControlPort 9051" >> /home/dietpi/tor/etc/torrc
-echo "CookieAuthentication 1" >> /home/dietpi/tor/etc/torrc
-echo "CookieAuthFile /home/dietpi/tor/data/control_auth_cookie" >> /home/dietpi/tor/etc/torrc
-echo "ControlSocket /home/dietpi/tor/data/control_socket" >> /home/dietpi/tor/etc/torrc
-echo "Address $(curl icanhazip.com)" >> /home/dietpi/tor/etc/torrc
-echo "ORPort 9001" >> /home/dietpi/tor/etc/torrc
-echo "ServerTransportPlugin obfs4 exec /home/dietpi/tor/bin/obfs4proxy" >> /home/dietpi/tor/etc/torrc
-echo "ServerTransportListenAddr obfs4 0.0.0.0:9002" >> /home/dietpi/tor/etc/torrc
-echo "ExtORPort auto" >> /home/dietpi/tor/etc/torrc
-echo "ContactInfo bridge@pizero.org" >> /home/dietpi/tor/etc/torrc
-echo "Nickname BRiDGEPiZERO" >> /home/dietpi/tor/etc/torrc
-echo "BandwidthRate 1024 KBytes" >> /home/dietpi/tor/etc/torrc
-echo "BandwidthBurst 1536 KBytes" >> /home/dietpi/tor/etc/torrc
-echo "MaxAdvertisedBandwidth 1280 KBytes" >> /home/dietpi/tor/etc/torrc
-echo "PublishServerDescriptor bridge" >> /home/dietpi/tor/etc/torrc
-echo "BridgeDistribution any" >> /home/dietpi/tor/etc/torrc
+echo "DataDirectory /home/debian-tor/tor/data" > /home/debian-tor/tor/etc/torrc
+echo "GeoIPFile /home/debian-tor/tor/data/geoip" >> /home/debian-tor/tor/etc/torrc
+echo "GeoIPv6File /home/debian-tor/tor/data/geoip6" >> /home/debian-tor/tor/etc/torrc
+echo "Log notice file /home/debian-tor/tor/log/notices.log" >> /home/debian-tor/tor/etc/torrc
+echo "PidFile /home/debian-tor/tor/tor.pid" >> /home/debian-tor/tor/etc/torrc
+echo "SocksPort 0" >> /home/debian-tor/tor/etc/torrc
+echo "BridgeRelay 1" >> /home/debian-tor/tor/etc/torrc
+echo "RunAsDaemon 1" >> /home/debian-tor/tor/etc/torrc
+echo "AvoidDiskWrites 1" >> /home/debian-tor/tor/etc/torrc
+echo "ControlPort 9051" >> /home/debian-tor/tor/etc/torrc
+echo "CookieAuthentication 1" >> /home/debian-tor/tor/etc/torrc
+echo "CookieAuthFile /home/debian-tor/tor/data/control_auth_cookie" >> /home/debian-tor/tor/etc/torrc
+echo "ControlSocket /home/debian-tor/tor/data/control_socket" >> /home/debian-tor/tor/etc/torrc
+echo "Address $(curl icanhazip.com)" >> /home/debian-tor/tor/etc/torrc
+echo "ORPort 9001" >> /home/debian-tor/tor/etc/torrc
+echo "ServerTransportPlugin obfs4 exec /home/debian-tor/tor/bin/obfs4proxy" >> /home/debian-tor/tor/etc/torrc
+echo "ServerTransportListenAddr obfs4 0.0.0.0:9002" >> /home/debian-tor/tor/etc/torrc
+echo "ExtORPort auto" >> /home/debian-tor/tor/etc/torrc
+echo "ContactInfo bridge@pizero.org" >> /home/debian-tor/tor/etc/torrc
+echo "Nickname BRiDGEPiZERO" >> /home/debian-tor/tor/etc/torrc
+echo "BandwidthRate 1024 KBytes" >> /home/debian-tor/tor/etc/torrc
+echo "BandwidthBurst 1536 KBytes" >> /home/debian-tor/tor/etc/torrc
+echo "MaxAdvertisedBandwidth 1280 KBytes" >> /home/debian-tor/tor/etc/torrc
+echo "PublishServerDescriptor bridge" >> /home/debian-tor/tor/etc/torrc
+echo "BridgeDistribution any" >> /home/debian-tor/tor/etc/torrc
 
 cd
 curl -L https://go.dev/dl/go1.21.6.linux-armv6l.tar.gz | tar zxf -
 curl -L https://gitlab.com/yawning/obfs4/-/archive/master/obfs4-master.tar.gz | tar zxf -
 cd obfs4-master
 ../go/bin/go build -o obfs4proxy/obfs4proxy ./obfs4proxy
-mv obfs4proxy/obfs4proxy /home/dietpi/tor/bin
-setcap cap_net_bind_service=+ep /home/dietpi/tor/bin/obfs4proxy
+mv obfs4proxy/obfs4proxy /home/debian-tor/tor/bin
+setcap cap_net_bind_service=+ep /home/debian-tor/tor/bin/obfs4proxy
 rm -r ../.cache/go-build
 
-chmod 600 /home/dietpi/tor/etc/torrc
-chown -R dietpi:dietpi /home/dietpi
+chmod 600 /home/debian-tor/tor/etc/torrc
+chown -R debian-tor:debian-tor /home/debian-tor
 
 systemctl daemon-reload
 systemctl enable tor.service
